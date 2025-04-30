a5d74332bdacc1c9438fde8a01106c3ad7556e68
bridgetorr17 <bridgetorr1902@gmail.com>
2025-04-30 19:35:32 -0400

    working on frontend

--- main.js
+++ main.js
@@ -1,73 +1,138 @@
-// try{
-//     const response = await fetch("localhost:8000/");
-//     const data = await response.json();
-
-//     console.log(data);
-// }
-// catch(error){
-//     console.error(error);
-// }
-try{
-    fetch("https://surprise-songs.vercel.app/")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        let list = document.getElementById('concertListRanked');
-        //add concert data from mongo to the DOM with javascript 
-        data.forEach((concert, index) => {
-            let li = document.createElement('li');
-            li.setAttribute('class', 'flex2');
-            list.appendChild(li);
-
-            let sec1 = document.createElement('section');
-            sec1.setAttribute('class', 'flex1 concertLeft');
-            li.appendChild(sec1);
-            
-            let h4 = document.createElement('h4');
-            h4.innerHTML = concert.concertName;
-            sec1.appendChild(h4);
-
-            let span1 = document.createElement('span');
-            span1.innerHTML = concert.guitarSong;
-            sec1.appendChild(span1);
-
-            let span2 = document.createElement('span');
-            span2.innerHTML = concert.pianoSong;
-            sec1.appendChild(span2);
-
-            let sec2 = document.createElement('section');
-            sec2.setAttribute('class', 'flex2 concertRight');
-            li.appendChild(sec2);
-
-            let img = document.createElement('img');
-            img.setAttribute('id', concert.concertName);
-            img.setAttribute('class', 'likeButton');
-            img.setAttribute('src', '/images/hearts.png');
-            img.setAttribute('alt', 'like button');
-            sec2.appendChild(img);
-
-            let span3 = document.createElement('span');
-            span3.setAttribute('id', concert.concertName.replace(/\s+/g, '_') + '_votes');
-            span3.setAttribute('class', 'bold');
-            span3.innerHTML = concert.votes;
-            sec2.appendChild(span3);
-        })
-    
-    })
-}
-catch(error){
-    console.error(error);
-}
-
-const likeButtons = document.querySelectorAll('.likeButton');
+//popup window logic and event listeners
 const popupButton = document.querySelector('#popup');
 const popupClose = document.querySelector('#popupClose');
 
-likeButtons.forEach(button => {
+popupButton.addEventListener('click', _ => {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("popupInfo").style.display = "block";
+});
+
+popupClose.addEventListener('click', _ => {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("popupInfo").style.display = "none";
+})
+
+//get page from server
+try{
+        fetch("https://surprise-songs.vercel.app")
+        .then(response => response.json())
+        .then(data => {
+            //add concert data already existing in the DB
+            data.forEach((concert) => {
+                addElement(concert);
+            });
+        })
+        .then(result => {
+            //add event listeners for all concert like buttons
+            const likeButtons = document.querySelectorAll('.likeButton');
+            likeButtons.forEach(button => {
+                addLikeButton(button);
+            })
+        })
+    }
+    catch(error){
+        console.error(error);
+    }
+
+//concert form POST with JS (handled here to prevent entire page reload)
+document.getElementById('concertForm').addEventListener('submit', async function(event){
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const formDataObj = {};
+    formData.forEach((value, key) => {
+        formDataObj[key] = value;
+    });
+
+    try{
+        const response = await fetch('https://surprise-songs.vercel.app/addConcert', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formDataObj)
+        });
+
+        if(response.ok){
+            formDataObj['votes'] = 0;
+            document.getElementById('concertForm').reset();
+            addElement(formDataObj);
+
+            const button = document.querySelector('ul').querySelectorAll('img');
+            buttonAdded = button[button.length-1]
+            addLikeButton(buttonAdded)
+        }
+    }
+    catch(error){
+        console.error(error);
+    }
+})
+
+//sort concerts on client side after like (could make a new request to the server also)
+function sortConcertList(){
+    let concertList = document.getElementById('concertListRanked');
+    let concertListArr = Array.from(concertList.children);
+
+    concertListArr.sort((a,b) => {
+        const aVotes = a.querySelector('span[id$="_votes"]').textContent;
+        const bVotes = b.querySelector('span[id$="_votes"]').textContent;
+        return bVotes - aVotes;
+    
+    });
+
+    concertListArr.forEach(item => concertList.appendChild(item));
+}
+
+//adding all HTML elements to concert list
+function addElement(concert){
+
+    let list = document.getElementById('concertListRanked');
+    let li = document.createElement('li');
+    li.setAttribute('class', 'flex2');
+    list.appendChild(li);
+
+    let sec1 = document.createElement('section');
+    sec1.setAttribute('class', 'flex1 concertLeft');
+    li.appendChild(sec1);
+    
+    let h4 = document.createElement('h4');
+    h4.innerHTML = concert.concertName;
+    sec1.appendChild(h4);
+
+    let span1 = document.createElement('span');
+    span1.innerHTML = concert.guitarSong;
+    sec1.appendChild(span1);
+
+    let span2 = document.createElement('span');
+    span2.innerHTML = concert.pianoSong;
+    sec1.appendChild(span2);
+
+    let sec2 = document.createElement('section');
+    sec2.setAttribute('class', 'flex2 concertRight');
+    li.appendChild(sec2);
+
+    let img = document.createElement('img');
+    img.setAttribute('id', concert.concertName);
+    img.setAttribute('class', 'likeButton');
+    img.setAttribute('src', '/images/hearts.png');
+    img.setAttribute('alt', 'like button');
+    sec2.appendChild(img);
+
+    let span3 = document.createElement('span');
+    span3.setAttribute('id', (concert.concertName.replace(/\s+/g, '_') + '_votes'));
+    span3.setAttribute('class', 'bold');
+    span3.innerHTML = concert.votes;
+    sec2.appendChild(span3);
+}
+
+//event listener for like button
+//where the PUT request is made to the server
+function addLikeButton(button){
     button.addEventListener('click', _ => {
+                    
         const concertName = button.id;
         
-        fetch('/like', {
+        fetch('https://surprise-songs.vercel.app/like', {
             method: 'PUT',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({concert: concertName})
@@ -87,28 +152,5 @@ likeButtons.forEach(button => {
             console.error("Error updaing item:", error);
         })
     })
-})
-
-function sortConcertList(){
-    let concertList = document.getElementById('concertListRanked');
-    let concertListArr = Array.from(concertList.children);
-
-    concertListArr.sort((a,b) => {
-        const aVotes = a.querySelector('span[id$="_votes"]').textContent;
-        const bVotes = b.querySelector('span[id$="_votes"]').textContent;
-        return bVotes - aVotes;
-    
-    });
-
-    concertListArr.forEach(item => concertList.appendChild(item));
 }
 
-popupButton.addEventListener('click', _ => {
-    document.getElementById("overlay").style.display = "block";
-    document.getElementById("popupInfo").style.display = "block";
-});
-
-popupClose.addEventListener('click', _ => {
-    document.getElementById("overlay").style.display = "none";
-    document.getElementById("popupInfo").style.display = "none";
-})
